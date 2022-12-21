2ae8d2cef7fe48259d1801eeeb280596221be591
Loïc Guillois <contact@loicguillois.fr>
2022-12-21 11:54:26 +0100

    update gitignore for data folder

---
+++ backend/test/data/users.js
@@ -0,0 +1,11 @@
+module.exports = {
+  USER_CEJ: {
+    email: 'suzette.apidet@email.fr',
+    password: 'supercej',
+  },
+
+  USER_ADMIN: {
+    email: 'passengagementjeune@beta.gouv.fr',
+    password: 'supersecret',
+  },
+};
